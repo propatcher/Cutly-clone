d88425929fc5c8dc9c65d9ee222d1730bb6ed238
propatcher <dfyz240206@gmail.com>
2025-09-22 17:04:12 +0400

    url validation

--- app/links/router.py
+++ app/links/router.py
@@ -5,6 +5,7 @@ from fastapi.responses import RedirectResponse
 
 from fastapi_cache import FastAPICache
 from fastapi_cache.decorator import cache
+from pydantic import AnyUrl
 from app.exceptions import TokenAbsentException
 from app.users.dependencies import get_current_user
 from app.users.models import User
@@ -16,20 +17,12 @@ router = APIRouter(
     tags=["Ссылки"]
 )
 
-def user_key_builder(func, *args, **kwargs):
-    current_user = kwargs.get('current_user')
-    if current_user:
-        return f"user_{current_user.id}_{func.__name__}"
-    return func.__name__
-
-
 @router.post("/create")
-async def create_short_link(link: str,current_user: User = Depends(get_current_user)):
-    if not current_user:
-        raise TokenAbsentException
+async def create_short_link(link:AnyUrl,current_user: User = Depends(get_current_user)):
+    link_str = str(link)
     all_symbols = string.ascii_letters + string.digits
     secure_random_string = ''.join(secrets.choice(all_symbols) for _ in range(10))
-    result = await LinksDAO.add_link(secure_random_string,link,current_user.id)
+    result = await LinksDAO.add_link(secure_random_string,link_str,current_user.id)
     return await LinksDAO.find_one_or_none(short_code=secure_random_string)
 
 
