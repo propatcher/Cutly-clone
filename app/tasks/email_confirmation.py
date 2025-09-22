913f932deb6afd76ceae2ffeb30081ee3886fff2
propatcher <dfyz240206@gmail.com>
2025-09-22 18:41:35 +0400

    full project docker v2

--- app/tasks/email_confirmation.py
+++ app/tasks/email_confirmation.py
@@ -7,10 +7,13 @@ from app.tasks.tasks import celery
 
 
 
-@celery.task
+@celery.task(max_retries=3)
 def send_booking_confirmation_email(email_to: EmailStr):
-    msg_content = create_user_confirmation_template(email_to)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT, context=context) as server:
-        server.login(settings.SMTP_USER, settings.SMTP_PASS)
-        server.send_message(msg_content)
+    try:
+        msg_content = create_user_confirmation_template(email_to)
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT, context=context) as server:
+            server.login(settings.SMTP_USER, settings.SMTP_PASS)
+            server.send_message(msg_content)
+    except (smtplib.SMTPServerDisconnected, smtplib.SMTPConnectError) as exc:
+        raise send_booking_confirmation_email.retry(exc=exc, countdown=60)
